6f23d214903271541be93e16c976da03a3bcba00
OskarZyg <56176746+OskarZyg@users.noreply.github.com>
2021-06-21 22:26:29 +0100

    Untested Initial Code

--- src/main/java/com/oskarsmc/broadcast/Broadcast.java
+++ src/main/java/com/oskarsmc/broadcast/Broadcast.java
@@ -1,31 +1,61 @@
 package com.oskarsmc.broadcast;
 
+import cloud.commandframework.execution.CommandExecutionCoordinator;
+import cloud.commandframework.velocity.CloudInjectionModule;
+import cloud.commandframework.velocity.VelocityCommandManager;
 import com.google.inject.Inject;
+import com.google.inject.Injector;
+import com.google.inject.Key;
+import com.google.inject.TypeLiteral;
 import com.oskarsmc.broadcast.command.BroadcastBrigadier;
+import com.oskarsmc.broadcast.command.BroadcastCommand;
+import com.velocitypowered.api.command.CommandSource;
 import com.velocitypowered.api.event.proxy.ProxyInitializeEvent;
 import com.velocitypowered.api.event.Subscribe;
 import com.velocitypowered.api.plugin.Plugin;
 import com.velocitypowered.api.proxy.ProxyServer;
+import org.bstats.velocity.Metrics;
 import org.slf4j.Logger;
 
-@Plugin(
-        id = "broadcast",
-        name = "Broadcast",
-        version = "0.1.0",
-        description = "Broadcasting Plugin For Velocity",
-        url = "https://software.oskarsmc.com/",
-        authors = {"OskarZyg", "OskarsMC"}
-)
+import java.util.function.Function;
+
 public class Broadcast {
 
     @Inject
-    private Logger logger;
+    public Logger logger;
 
     @Inject
-    private ProxyServer proxyServer;
+    public ProxyServer proxyServer;
+
+    @Inject
+    private Metrics.Factory metricsFactory;
+
+    @Inject
+    private Injector injector;
+
+
+    public VelocityCommandManager<CommandSource> commandManager;
+    public BroadcastCommand broadcastCommand;
+    public Metrics metrics;
 
     @Subscribe
     public void onProxyInitialization(ProxyInitializeEvent event) {
-        new BroadcastBrigadier(proxyServer);
+        final Injector childInjector = injector.createChildInjector(
+                new CloudInjectionModule<>(
+                        CommandSource.class,
+                        CommandExecutionCoordinator.simpleCoordinator(),
+                        Function.identity(),
+                        Function.identity()
+                )
+        );
+
+        this.commandManager = childInjector.getInstance(
+                Key.get(new TypeLiteral<VelocityCommandManager<CommandSource>>() {
+                })
+        );
+
+        this.metrics = metricsFactory.make(this, 11783);
+
+        this.broadcastCommand = new BroadcastCommand(this);
     }
 }
